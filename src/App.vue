4e2ec9249207fa03362fc6f82298bb2aa66ee625
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2023-01-03 21:08:26 -0500

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
 // escribir javascript
 const name = 'Maria dinamico'
+const styleColor = 'color: blue' 
 </script>
 
 <template>
     <h1>hola {{name}}</h1>
+    <br>
+    <h2 style="color:blue">Soy azul</h2>
+    <br>
+    <h2 v-bind:style="styleColor">Soy azul</h2>
+    <h2 :style="styleColor">Soy azul</h2>
 </template>
 
 <style>
