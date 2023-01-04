dca1bc2213797565193eac0c1e2d4219f37f6720
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2023-01-04 08:20:03 -0500

    v-if and v-for together

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,19 @@ const arrayFrutas2 = [
             name: "Manzana",
             price: "$1.00",
             description: "Una manzana",
+            stock: 0,
         },
         {
             name: "Pera",
             price: "$2.00",
             description: "Una pera",
+            stock: 10,
         },
         {
             name: "Naranja",
             price: "$3.00",
             description: "Una naranja",
+            stock: 20,
         },
     ];
 const fruta = {
@@ -31,43 +34,68 @@ const fruta = {
 </script>
 
 <template>
+  <div class="container">
     <h1>hola {{name}}</h1>
-    <br>
+    <hr>
     <h1>hola {{name.toLocaleUpperCase()}}</h1>
-    <br>
+    <hr>
     <h2 style="color:blue">Soy azul</h2>
-    <br>
+    <hr>
     <h2 v-bind:style="styleColor">Soy azul</h2>
-    <br>
+    <hr>
     <h2 :style="styleColor">Soy azul</h2>
-    <br>
+    <hr>
     <h2 :style="`color: ${arrayColores[2]}`">Soy Peru</h2>
-    <br>
+    <hr>
     <h2>{{activo? 'Estoy activo' : 'Estoy inactivo'}}</h2>
-    <br>
+    <hr>
     <h2 v-if="!inactivo">{{'Estoy inactivo'}}</h2>
     <h2 v-else>{{'Estoy Activo'}}</h2>
-    <br>
+    <hr>
     <ul>
         <li v-for="(fruta, index) in arrayFrutas" :key="index">
             {{ fruta }}
         </li>
     </ul>
-    <br>
+    <hr>
     <ul>
         <li v-for="fruta in arrayFrutas2" :key="fruta.name">
             {{ fruta.name }} - {{ fruta.price }} - {{ fruta.description }}
         </li>
     </ul>
-    <br>
+    <hr>
     <ul>
         <li v-for="(value, propiedad, index) in fruta" :key="index">
             {{ propiedad }} -> {{ value }} -> {{index}}
         </li>
     </ul>
+    <hr>
+    <ul>
+        <li
+            v-for="fruta in arrayFrutas2"
+            :key="fruta.name"            
+        >
+        <!-- No muy eficaz -->
+            <p v-if="fruta.stock > 0">{{ fruta }}</p>
+        </li>
+    </ul>
+    <hr>
+    <ul>
+      <!-- solución -->
+    <template v-for="fruta in arrayFrutas2" :key="fruta.name">
+        <li v-if="fruta.stock > 0">
+            {{ fruta }}
+        </li>
+    </template>
+</ul>
+  </div>
 </template>
 
 <style>
+.container {
+  margin: 0 auto;
+  width: 500px;
+}
 h1 {
   color: blueviolet;
 }
