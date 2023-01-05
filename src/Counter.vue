5ec9f1eefcc9d6094ad4fccb230befbae6e0d598
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2023-01-05 11:55:40 -0500

    update counter with practice

--- src/Counter.vue
+++ src/Counter.vue
@@ -4,6 +4,7 @@ import { ref, computed } from "vue";
 const name = "Vue 3";
 
 const counter = ref(0);
+const arrayCounter = ref([]);
 
 const increment = () => {
     counter.value++;
@@ -17,13 +18,21 @@ const reset = () => {
     counter.value = 0;
 };
 
+const add = () => {
+    arrayCounter.value.push(counter.value);
+};
+
+const blockNumber = computed(() => {
+    const number = arrayCounter.value.find((num) => num === counter.value);
+    return number || number === 0;
+});
+
 const classCounter = computed(() => {
     if (counter.value === 0) {
         return "zero";
     }
     return counter.value > 0 ? "positive" : "negative";
 });
-
 </script>
 
 <template>
@@ -34,6 +43,12 @@ const classCounter = computed(() => {
     <button @click="increment">Incremet</button>
     <button @click="decrement">Decrement</button>
     <button @click="reset">Reset</button>
+    <button @click="add" :disabled="blockNumber">Add</button>
+    <ul>
+        <li v-for="(item, index) in arrayCounter" :key="index">
+            {{ item }}
+        </li>
+    </ul>
 </template>
 
 <style>
@@ -46,6 +61,6 @@ const classCounter = computed(() => {
 }
 
 .zero {
-    color: peru;
+    color: black;
 }
 </style>
