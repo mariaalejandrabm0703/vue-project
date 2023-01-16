4fac6b750ed3d1351316be1c55bd4328fefe984e
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2023-01-16 10:45:42 -0500

    curso vue 3 - componentes CLI

---
+++ src/components/Practice1.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+const name = "Vue 3";
+</script>
+
+<template>
+    <h1>Hola {{ name }}!</h1>
+</template>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Counter from './Counter.vue'
+import Counter from './components/Counter.vue'
+import Practice1 from './components/Practice1.vue'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'material-design-icons/iconfont/material-icons.css'
 
 import './assets/main.css'
 
-createApp(Counter).mount('#app')
+// createApp(App).mount('#app')
+// createApp(Counter).mount('#app')
+createApp(Practice1).mount('#app')
